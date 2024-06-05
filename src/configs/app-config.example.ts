d2156926a721f5cac089c3a5827989a883f238c4
Joseph Chingalo <profschingalo@gmail.com>
2024-06-05 12:06:26 +0300

    chore: :hammer: set configs and constant for script

---
+++ src/configs/app-config.example.ts
@@ -0,0 +1,7 @@
+import { AppConfigModel } from '../models';
+
+export const appSourceConfig: AppConfigModel = {
+  username: 'dhis_username',
+  password: 'dhis_password',
+  baseUrl: 'dhis_base_url'
+};
