95b7c4220dff1330b1c39e9813ec5195f207afa4
Joseph Chingalo <profschingalo@gmail.com>
2024-06-06 10:08:39 +0300

    chore: :hammer: updating app configs

--- src/configs/app-config.example.ts
+++ src/configs/app-config.example.ts
@@ -9,3 +9,8 @@ export const appSourceConfig: AppConfigModel = {
 export const DHIS2_ORGANISATION_UNIT_CONSTANT = {
   validationRuleOuLevel: 1 // level for ous used for organunits, ouId will be fetched by level
 };
+
+export const DHIS2_VALIDATION_RULE_CONSTANT = {
+  validationRuleGroup: '', // id for validation rul group for the notifications
+  defaultNumberOfMonth: 2 // number of month including current month get end date
+};
