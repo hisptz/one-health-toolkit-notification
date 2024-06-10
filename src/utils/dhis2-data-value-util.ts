2a2b4d6646a90e899c084710bdb5cfd9c6f7266a
Joseph Chingalo <profschingalo@gmail.com>
2024-06-10 15:31:27 +0300

    feat: :sparkles: util fnc for aggregate data values

--- src/utils/dhis2-data-value-util.ts
+++ src/utils/dhis2-data-value-util.ts
@@ -1,22 +1,49 @@
-import { filter, chunk, join, uniq } from 'lodash';
+import { filter, chunk, join, uniq, groupBy, keys } from 'lodash';
 import { AppUtil, HttpUtil, LogsUtil } from '.';
 import { Dhis2DataValue, Dhis2dataVelueResponse } from '../models';
 
 export class Dhis2DataValueUtil {
   private _url: string;
   private _headers: { 'Content-Type': string; Authorization: string };
-  private _dataValueFileName: string;
-  private _dataValueDir: string = 'data-values';
   private _dataSyncPageSize: number = 500;
 
   constructor(username: string, password: string, baseUrl: string) {
-    this._dataValueFileName = `Data values`;
     this._url = baseUrl;
     this._headers = AppUtil.getHttpAuthorizationHeader(username, password);
   }
 
-  get dataValueDir(): string {
-    return this._dataValueDir;
+  async getAggregatedDatavalues(
+    dhis2DataValues: Dhis2DataValue[]
+  ): Promise<Dhis2DataValue[]> {
+    const formattedDhis2DataValues: Dhis2DataValue[] = [];
+    try {
+      const groupedOus = groupBy(dhis2DataValues, 'orgUnit');
+      for (const orgUnit of keys(groupedOus)) {
+        const groupedPeriods = groupBy(groupedOus[orgUnit], 'period');
+        for (const period of keys(groupedPeriods)) {
+          const groupedDataElements = groupBy(
+            groupedPeriods[period],
+            'dataElement'
+          );
+          for (const dataElement of keys(groupedDataElements)) {
+            const value: any = groupedDataElements[dataElement].length ?? 0;
+            formattedDhis2DataValues.push({
+              dataElement,
+              period,
+              orgUnit,
+              value
+            });
+          }
+        }
+      }
+    } catch (error: any) {
+      await new LogsUtil().addLogs(
+        'error',
+        error.message || error,
+        'Dhis2DataValueUtil'
+      );
+    }
+    return formattedDhis2DataValues;
   }
 
   async syncDataValues(
